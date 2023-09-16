a27025d15ee7ab97227f6f7670d2b40e66f2a9e5
Mahmo <midoahme93@gmail.com>
2023-09-16 16:25:38 +0300

    Finished Radio Tap

--- lib/tabs/radioTab.dart
+++ lib/tabs/radioTab.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/myThemeData.dart';
 
 class RadioTab extends StatelessWidget {
   const RadioTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Padding(
+      padding: const EdgeInsets.only(top: 150),
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset("assets/images/radio_image.png"),
+            SizedBox(
+              height: 20,
+            ),
+            Text("إذاعة القران الكريم",style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+              color: MyThemeData.blackColor
+            ),),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+               ImageIcon(AssetImage("assets/images/prev.png",),color: MyThemeData.primaryColor,),
+                ImageIcon(AssetImage("assets/images/play.png"),color: MyThemeData.primaryColor,),
+                ImageIcon(AssetImage("assets/images/next.png"),color: MyThemeData.primaryColor,)
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
