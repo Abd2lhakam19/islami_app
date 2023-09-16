5b1f7849e8532e27e8a5727d103fc4139beb8810
Mahmo <midoahme93@gmail.com>
2023-09-16 17:21:52 +0300

    Finished sebha Tap

--- lib/tabs/sebhaTab.dart
+++ lib/tabs/sebhaTab.dart
@@ -1,10 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/myThemeData.dart';
 
-class SebhaTab extends StatelessWidget {
-  const SebhaTab({super.key});
+class SebhaTab extends StatefulWidget {
+  @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  int counter = 0;
+  int index = 0;
+  List<String> azkar = [
+    'سبحان الله',
+    'الحمد لله ',
+    'لا اله الا الله',
+    'الله اكبر '
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset("assets/images/sebhabody.png"),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              "عدد التسبيحات",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(color: MyThemeData.blackColor),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 65,
+              width: 50,
+              decoration: BoxDecoration(
+                  color: MyThemeData.primaryColor.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(14)),
+              child: Center(
+                  child: Text(
+                "$counter",
+                style: Theme.of(context).textTheme.bodySmall,
+              )),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    fixedSize: Size(140, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    backgroundColor: MyThemeData.primaryColor),
+                onPressed: () {
+                  counter++;
+                  if (counter > 33) {
+                    index++;
+                    counter = 0;
+                    if(index==azkar.length-1)
+                      {
+                        index = 0;
+                      }
+                  }
+                  setState(() {});
+                },
+                child: Text(
+                  "${azkar[index]}",
+                  style: Theme.of(context).textTheme.bodySmall,
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
